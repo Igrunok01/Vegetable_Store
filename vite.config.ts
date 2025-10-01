78ae01c9040f1c0493f524601c2dd9bf54cd2907
Dan <igrunok02@gmail.com>
2025-10-02 01:27:00 +0300

    update with RTK-

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/ReactViteTS/',
+  base: '/Vegetable_Store/',
   plugins: [react()],
   test: {
     globals: true,
